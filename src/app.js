b0765a5aff1b7b3890496f9cfd8492cb6bbcd11f
Kam Lasater <kam@cyclic.sh>
2021-07-02 14:54:29 -0400

    Changed variable name to match Cyclic prefix

--- src/app.js
+++ src/app.js
@@ -2,8 +2,8 @@ const assert = require('assert');
 const Provider = require('oidc-provider');
 
 // process.env.SECURE_KEY contains two secret strigs seperated by comma => current_key_str,old_key_str
-assert(process.env.SECURE_KEY, 'process.env.SECURE_KEY missing, make sure your env is configured correctly');
-assert(process.env.SECURE_KEY.split(',').length === 2, 'process.env.SECURE_KEY format invalid');
+assert(process.env.cyclic_app_env_SECURE_KEY, 'process.env.SECURE_KEY missing, make sure your env is configured correctly');
+assert(process.env.cyclic_app_env_SECURE_KEY.split(',').length === 2, 'process.env.SECURE_KEY format invalid');
 
 // new Provider instance with no extra configuration, will run in default, just needs the issuer
 // identifier, uses data from runtime-dyno-metadata heroku here
@@ -18,7 +18,7 @@ const oidc = new Provider(`https://api.cyclic.sh`, { // Probably want to overwri
     },
   ],
   cookies: {
-    keys: process.env.SECURE_KEY.split(','),
+    keys: process.env.cyclic_app_env_SECURE_KEY.split(','),
   },
 });
 
